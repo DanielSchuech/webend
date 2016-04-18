5827ebf05cb305250cbd3c428645169def279eca
Daniel Schuech <dschuech@gmail.com>
2016-04-18 08:27:06 +0200

    feat: dashboard layout

--- src/controlapp/layout/dashboard/dashboard.component.ts
+++ src/controlapp/layout/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'status',
-  template: 'status text'
+  template: require('./dashboard.html')
 })
 export class DashboardComponent {}
 
